66714a19060ee259720d275cc14562c14cae2244
Marian C Moldovan <marian.claudiu@beeva.com>
2017-03-03 17:38:45 +0100

    Basic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+    var data = [
+      {
+        'nombre': 'Marian C. Moldovan',
+        'foto': 'https://farm6.staticflickr.com/5607/29838407936_5c1199449d_k_d.jpg',
+        'amarillo': '',
+        'rojo': '',
+        'negro': ''
+      }
+    ];
+
+    function setStyle(style) {
+      $('#left').addClass(style);
+      $('#top').addClass(style);
+      $('#bottom').addClass(style);
+      $('#right').addClass(style);
+    }
+
+    setStyle('amarillo');
+
+
 });
