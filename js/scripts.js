586666052c1b732ba37724afae106fde0bb1f0fe
Marian C Moldovan <marian.claudiu@beeva.com>
2017-03-03 18:45:21 +0100

    Shit

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,14 @@ $( document ).ready(function() {
       $('#top').addClass(style);
       $('#bottom').addClass(style);
       $('#right').addClass(style);
+      $('#fondito').addClass('fondito' + style);
+      $('#textito').addClass('textito' + style);
     }
 
     function setData(style){
       var person = random();
       $('#textito').html('<span id="fondito">' + person[style] + '</span>');
       $('#fotito').attr('src', person['foto']);
-      $('#fondito').addClass('fondito' + style);
-      $('#textito').addClass('textito' + style);
     }
 
     var color = location.search.split('color=')[1];
