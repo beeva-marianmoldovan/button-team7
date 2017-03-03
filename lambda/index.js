a17af4a5fecbc1fd413cd061f1a4a14680266439
Marian C Moldovan <marian.claudiu@beeva.com>
2017-03-03 17:52:52 +0100

    Lambda

---
+++ lambda/index.js
@@ -0,0 +1,31 @@
+const request = require('request');
+
+exports.handler = (event, context, callback) => {
+    console.log('Received event:', event);
+
+    var color = 'amarillo';
+    if(event.clickType === 'LONG') {
+      color = 'rojo';
+      console.log('Un click');
+    }
+    else if(event.clickType === 'DOUBLE'){
+      color = 'negro';
+      console.log('Un click doble');
+    }
+
+    var options = {
+      uri: 'https://c67cac8a2eae1d04f5928b5b1603a36ae49eafede475c07838e278610d7e0a.resindevice.io/content',
+      method: 'POST',
+      json: {
+      	'content': 'https://beeva-marianmoldovan.github.io/button-team7?color=' + color,
+      	'screen': 'Labs.2',
+      	'type': 'web'
+      }
+    };
+    request('http://www.google.com', function (error, response, body) {
+      context.done(err, response);
+    });
+
+};
+
+exports.handler({clickType:'Single'}, {}, {})
